690ce3dca3fe3783a9473a10e659aee7fc047f34
Piotr Zieliński <piotrekzie100@gmail.com>
2022-12-13 22:33:19 +0100

    Updates for experiments

--- src/utils/util.py
+++ src/utils/util.py
@@ -13,7 +13,7 @@ import argparse
 import torch
 import torch.nn as nn
 import torch.optim as optim
-import visdom
+# import visdom
 import numpy as np
 from torch.autograd import Variable
 from torch.utils.data import DataLoader
